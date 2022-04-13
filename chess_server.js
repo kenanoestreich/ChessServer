1d5db2518e9f44ebba18002c142fb8c0967ecb92
kenanoestreich <kenan.oestreich@wustl.edu>
2022-04-13 16:30:56 -0500

    trying to get node set up first

---
+++ chess_server.js
@@ -0,0 +1,7 @@
+// Require the packages we will use:
+const http = require("http"),
+      fs = require("fs");
+
+const port = 3456;
+
+const file = "/home/kenanoestreich/module6/chess.html"; // FOR KENAN'S USE DO NOT DELETE
