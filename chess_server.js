655a2db5b5f72e8fef2fa4dad1fb263532461fb5
kenanoestreich <kenan.oestreich@wustl.edu>
2022-04-13 16:32:59 -0500

    commit

--- chess_server.js
+++ chess_server.js
@@ -4,4 +4,18 @@ const http = require("http"),
 
 const port = 3456;
 
-const file = "/home/kenanoestreich/module6/chess.html"; // FOR KENAN'S USE DO NOT DELETE
+const file = "/home/kenanoestreich/module6/chess.html"; 
+
+// Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html, on port 3456:
+const server = http.createServer(function (req, res) {
+    // This callback runs when a new connection is made to our HTTP server.
+
+    fs.readFile(file, function (err, data) {
+        // This callback runs when the client.html file has been read from the filesystem.
+
+        if (err) return res.writeHead(500);
+        res.writeHead(200);
+        res.end(data);
+    });
+});
+server.listen(port);
